879660eea2eb0cc66114f3ec0bf310e57c459397
Prasanth Varghese <mail2prasanthv@gmail.com>
2019-02-12 21:59:32 +0530

    Added new file

--- src/main/java/com/example/aop/aopproject/aspect/BusinesAspects.java
+++ src/main/java/com/example/aop/aopproject/aspect/BusinesAspects.java
@@ -1,38 +1,43 @@
 package com.example.aop.aopproject.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
 public class BusinesAspects {
 
-	// "copy Qualified Name option" is available to copy the method signature
-	@Around("com.example.aop.aopproject.aspect.CommonPointCutConfig.businessLayerPointCut()")
-	public void aroundAspect(ProceedingJoinPoint point) throws Throwable {
-		System.out.println("--Before Method Execution----");
-		long startTime = System.currentTimeMillis();
+    // "copy Qualified Name option" is available to copy the method signature
+    @Around("com.example.aop.aopproject.aspect.CommonPointCutConfig.businessLayerPointCut()")
+    public void aroundAspect(ProceedingJoinPoint point) throws Throwable {
+        System.out.println("--Before Method Execution----");
+        long startTime = System.currentTimeMillis();
 
-		Object proceed = point.proceed();// Proceeds with method execution
+        Object proceed = point.proceed();// Proceeds with method execution
 
-		long endTime = System.currentTimeMillis();
-		System.out.println("--After Method Execution----" + proceed);
+        long endTime = System.currentTimeMillis();
+        System.out.println("--After Method Execution----" + proceed);
 
-		System.out.println("Time Taken to execute(in ms): " + (endTime - startTime));
-	}
+        System.out.println("Time Taken to execute(in ms): " + (endTime - startTime));
+    }
 
-//	@After(value = "execution (* com.example.aop.aopproject.business.*.*(..))")
-//	public void afterAspect(JoinPoint point) {
-//		System.out.println("After Call--  Calls Intercepted:" + point);
-//	}
+    @After(value = "execution (* com.example.aop.aopproject.business.*.*(..))")
+    public void afterAspect(JoinPoint point) {
+        System.out.println("After Call--  Calls Intercepted:" + point);
+    }
+
 //
-//	@AfterThrowing(value = "execution (* com.example.aop.aopproject.business.*.*(..))", throwing = "exception")
-//	public void afterThrowingAspect(JoinPoint point, Object exception) {
-//		System.out.println("After Returning  Calls Intercepted:" + point);
-//		System.out.println("Eception Value: " + exception);
-//	}
+    @AfterThrowing(value = "execution (* com.example.aop.aopproject.business.*.*(..))", throwing = "exception")
+    public void afterThrowingAspect(JoinPoint point, Object exception) {
+        System.out.println("After Returning  Calls Intercepted:" + point);
+        System.out.println("Eception Value: " + exception);
+    }
 //
 //	@AfterReturning(value = "execution (* com.example.aop.aopproject.business.*.*(..))", returning = "result")
 //	public void afterAspect(JoinPoint point, Object result) {
@@ -40,14 +45,14 @@ public class BusinesAspects {
 //		System.out.println("Returned Value: " + result);
 //	}
 
-	// Format (execution(* Package-Name.*.*(..))
-	// This will intercept for all classes and
-	// methods in the package(com.example.aop.aopproject.business).
-	// (..) represents methods with any number of arguments.
-//	@Before("execution (* com.example.aop.aopproject.business.*.*(..))")
-//	public void beforeAspect(JoinPoint point) {
-//		System.out.println("Before -- Calls Intercepted:" + point);
-//		System.out.println("Checking User Access...");
-//		System.out.println("User Access allowed...");
-//	}
+    // Format (execution(* Package-Name.*.*(..))
+    // This will intercept for all classes and
+    // methods in the package(com.example.aop.aopproject.business).
+    // (..) represents methods with any number of arguments.
+    @Before("execution (* com.example.aop.aopproject.business.*.*(..))")
+    public void beforeAspect(JoinPoint point) {
+        System.out.println("Before -- Calls Intercepted:" + point);
+        System.out.println("Checking User Access...");
+        System.out.println("User Access allowed...");
+    }
 }
